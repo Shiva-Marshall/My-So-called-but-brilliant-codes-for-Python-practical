99b8eead508c6ef49353ed6430ce4957c300a34f
Shiva-Marshall <120257799+Shiva-Marshall@users.noreply.github.com>
2022-12-10 18:42:46 +0530

    Add files via upload

---
+++ Check a number, or alphabhet or special character.py
@@ -0,0 +1,9 @@
+a = input("enter anything:: ")
+x = a.isalpha()
+if x==True:
+    print("It's an alphabhet")
+elif x==issubclass:
+        print("it's an special character")
+else:
+    print("It's an number or special character")
+    
