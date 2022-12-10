5bc430be1f481f5dae9c76cb3520cc8601521854
Shiva-Marshall <120257799+Shiva-Marshall@users.noreply.github.com>
2022-12-10 20:33:37 +0530

    Add files via upload

---
+++ find min and max in tuple.py
@@ -0,0 +1,4 @@
+a = (2,3,4,24,432343,2342342,2432,)
+values = list(a)
+print('The maximum value in the concatenated tuple is: ' + str(max(values)))
+print('The minimum value in the concatenated tuple is: ' + str(min(values)))
