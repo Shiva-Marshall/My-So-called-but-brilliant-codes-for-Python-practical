f501901ecfddbbb788e5c702b0dbb9bd539275db
Shiva-Marshall <120257799+Shiva-Marshall@users.noreply.github.com>
2022-12-10 20:27:49 +0530

    Add files via upload

--- even numbers of the tuple of 1st string.py
+++ even numbers of the tuple of 1st string.py
@@ -6,8 +6,3 @@ for i in t1:
         test.append(i)
 test_tup = tuple(test)
 print(test_tup)
-# c)
-t2 = (11, 13, 15)
-conc = t1 + t2
-print(type(conc))
-print('The concatenation of t2 is: ' + str(conc))
